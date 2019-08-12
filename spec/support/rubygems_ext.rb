0e8e24a0288650391a46d40eb6644f22bd5079d9
David Rodríguez <deivid.rodriguez@riseup.net>
2019-08-12 23:30:45 +0200

    Lazily require rubygems/user_interaction

--- spec/support/rubygems_ext.rb
+++ spec/support/rubygems_ext.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "rubygems/user_interaction"
 require_relative "path"
 require "fileutils"
 
@@ -71,6 +70,7 @@ module Spec
       ENV["HOME"] = Path.home.to_s
       ENV["TMPDIR"] = Path.tmpdir.to_s
 
+      require "rubygems/user_interaction"
       Gem::DefaultUserInteraction.ui = Gem::SilentUI.new
     end
 
